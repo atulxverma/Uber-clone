3e636801db5a4cc4522d28df736d98d3b29daac0
Atul Verma <atulv9926@gmail.com>
2026-03-04 02:11:10 +0530

    Registered admin routes in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
+
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
 const mapsRoutes = require('./routes/maps.routes');
@@ -25,6 +27,7 @@ app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapsRoutes);
 app.use('/rides', rideRoutes);
-app.use('/admin', adminRoutes); 
+
+app.use('/admin', adminRoutes);
 
 module.exports = app;
